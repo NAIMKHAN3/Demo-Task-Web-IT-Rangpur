b255b9b197115557c4efb6a40c665ee24e7af3b5
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-03-18 19:06:52 +0600

    initial setup success

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './Router/router';
 
 function App() {
   return (
     <div>
       <h1 className='text-4xl font-bold text-indigo-700 text-center'>Demo Task Web IT Rangpur</h1>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
